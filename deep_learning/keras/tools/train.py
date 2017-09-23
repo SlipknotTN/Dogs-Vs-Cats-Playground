2042d4d6a7ea8073dcbab1d5956844abb06886eb
SlipknotTN <micheletoni87@tin.it>
2017-09-23 18:39:18 +0200

    Keras example wip and dataset split utility

--- deep_learning/keras/tools/train.py
+++ deep_learning/keras/tools/train.py
@@ -1,25 +1,19 @@
 import argparse
 
 from keras.models import Sequential
-from keras.layers import Conv2D
+from keras.layers import Conv2D, Flatten
 from keras.layers.pooling import AveragePooling2D
-from keras.applications.mobilenet import MobileNet
+from keras.applications.mobilenet import MobileNet, preprocess_input
+from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
-
-
-def preprocessing(image):
-    """
-    MobileNet preprocessing
-    """
-    image /= 255.0
-    image -= 0.5
-    image *= 2.0
-    return image
 
 
 def doParsing():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--datasetDir", required=True, type=str, help="Dataset directory")
+    parser.add_argument("--datasetTrainDir", required=True, type=str, help="Dataset train directory")
+    parser.add_argument("--datasetValDir", required=True, type=str, help="Dataset validation directory")
+    parser.add_argument("--batchSize", required=False, type=int, default=16, help="Batch size")
+    parser.add_argument("--epochs", required=False, type=int, default=50, help="Number of training epochs")
     args = parser.parse_args()
     return args
 
@@ -36,19 +30,56 @@ def main():
                           weights='imagenet', input_tensor=None, pooling=None)
 
     fineTunedModel = Sequential()
+
     fineTunedModel.add(baseModel)
 
     # Global average output shape (None, 1, 1, 1024).
     # Global pooling with AveragePooling2D to have a 4D Tensor and apply Conv2D.
-    fineTunedModel.add(AveragePooling2D(pool_size=(7, 7), strides=(1, 1), padding='valid', data_format=None))
+    fineTunedModel.add(AveragePooling2D(pool_size=(7, 7), strides=(1, 1), padding='valid', name="global_pooling"))
 
-    # Convolution layer like TF MobileNet with 2 classes, output shape (None, 1, 1, 2)
-    fineTunedModel.add(Conv2D(filters=2, kernel_size=(1,1), activation=None))
+    # Convolution layer like TF MobileNet, with 2 classes, output shape (None, 1, 1, 2)
+    fineTunedModel.add(Conv2D(filters=2, kernel_size=(1,1), activation=None, name="fc_conv"))
 
-    # Image Generator, MobileNet needs [-1.0, 1.0] range
-    trainImageGenerator = ImageDataGenerator(preprocessing_function=preprocessing, horizontal_flip=True)
-    valImageGenerator = ImageDataGenerator(preprocessing_function=preprocessing)
+    # Reshape to (None, 2) to match the one hot encoding target
+    fineTunedModel.add(Flatten())
 
+    # Freeze the base model layers, train only the last convolution
+    for layer in fineTunedModel.layers[0].layers:
+        layer.trainable = False
+
+    fineTunedModel.compile(loss='categorical_crossentropy',
+                           optimizer=optimizers.SGD(lr=1e-3, momentum=0.9),
+                           metrics=['accuracy'])
+
+    # Image Generator, MobileNet needs [-1.0, 1.0] range (Inception like preprocessing)
+    trainImageGenerator = ImageDataGenerator(preprocessing_function=preprocess_input, horizontal_flip=True)
+    valImageGenerator = ImageDataGenerator(preprocessing_function=preprocess_input)
+
+    trainGenerate = trainImageGenerator.flow_from_directory(
+        args.datasetTrainDir,
+        # height, width
+        target_size=(224, 224),
+        batch_size=args.batchSize,
+        class_mode='categorical',
+        shuffle=True)
+
+    valGenerate = valImageGenerator.flow_from_directory(
+        args.datasetValDir,
+        # height, width
+        target_size=(224, 224),
+        batch_size=args.batchSize,
+        class_mode='categorical',
+        shuffle=False)
+
+    # fine-tune the model
+    fineTunedModel.fit_generator(
+        trainGenerate,
+        steps_per_epoch=trainGenerate.samples//trainGenerate.batch_size,
+        epochs=args.epochs,
+        validation_data=valGenerate,
+        validation_steps=valGenerate.samples//valGenerate.batch_size)
+
+    print("End")
 
 if __name__ == '__main__':
     main()
