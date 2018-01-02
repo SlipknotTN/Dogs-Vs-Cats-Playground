11e0e7b48fbcc2d235ae019e9e03057ade504318
SlipknotTN <micheletoni87@tin.it>
2018-01-02 16:20:03 +0100

    Removed meaningless print error

--- deep_learning/tensorflow/lib/model/TensorflowModel.py
+++ deep_learning/tensorflow/lib/model/TensorflowModel.py
@@ -12,15 +12,7 @@ class TensorflowModel(object):
         # Load the saved graph
         with gfile.GFile(modelPath, 'rb') as f:
             graph_def = tf.GraphDef()
-            try:
-                graph_def.ParseFromString(f.read())
-            except:
-                print('try adding PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python' +
-                      'to environment.  e.g.:\n' +
-                      'PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python ipython\n' +
-                      'See here for info: ' +
-                      'https://github.com/tf/tf/issues/582')
-
+            graph_def.ParseFromString(f.read())
             self.sess = tf.Session()
             tf.import_graph_def(graph_def, name="")
 
